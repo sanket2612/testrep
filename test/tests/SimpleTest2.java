49e78b6710b2bb3b2a27d2e469cef3f60dd61e6e
sanket <sanket@Sanket-Laptop>
2015-05-26 11:38:19 +0530

    breakup into stages

---
+++ test/tests/SimpleTest2.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sanket
+ */
+public class SimpleTest2 {
+    
+    public SimpleTest2() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    
+     @Test
+     public void hello2() {
+         assertEquals("1", "2");
+     }
+}
